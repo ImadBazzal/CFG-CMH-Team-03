f550b76e978e2b2818fe3d72847ba1b4158bc929
Meenakshi Varadarajan <meenakshi.var3@gmail.com>
2025-11-08 00:40:11 -0500

    map feature: displays college markers from the geocoded JSON data

--- src/components/CollegeMap.jsx
+++ src/components/CollegeMap.jsx
@@ -1,13 +1,29 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import collegesData from "../data/university_data_geocoded.json";
 
-const CollegeMap = () => (
-  <MapContainer center={[39.5, -98.35]} zoom={4} style={{ height: "100vh", width: "100%" }}>
-    <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    />
-  </MapContainer>
-);
+const CollegeMap = () => {
+  return (
+    <div style={{ height: "80vh", width: "100%" }}>
+      <MapContainer center={[39.8283, -98.5795]} zoom={4}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+        />
+
+        {collegesData.map((college, idx) =>
+          college.lat && college.lng ? (
+            <Marker key={idx} position={[college.lat, college.lng]}>
+              <Popup>
+                <b>{college["School Name"]}</b><br />
+                {college.City}, {college.State}
+              </Popup>
+            </Marker>
+          ) : null
+        )}
+      </MapContainer>
+    </div>
+  );
+};
 
 export default CollegeMap;
